36aad21c10bb55a16ab8f4a339c4e090f191d20a
JanardanPandey <janardanpandey0510@gmail.com>
2021-09-22 15:47:33 +0530

    views function added for login, logout, profile

--- rbp2/rbpApp/views.py
+++ rbp2/rbpApp/views.py
@@ -22,20 +22,35 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
 
 def user_login(request):
-    if request.method == "POST":
-        fm = AuthenticationForm(request=request, data = request.POST)
-        if fm.is_valid():
-            uname=fm.cleaned_data['username']
-            upass = fm.cleaned_data['password']
-            user = authenticate(username = uname, password=upass)
-            if user is not None:
-                login(request, user)
-                return HttpResponseRedirect('/profile/')
-    else:
-        fm = AuthenticationForm()
-    return render(request, 'enroll/userlogin.html',{'forms':fm})
+    if not request.user.is_authenticated:
+        if request.method == "POST":
+            fm = AuthenticationForm(request=request, data = request.POST)
+            if fm.is_valid():
+                uname=fm.cleaned_data['username']
+                upass = fm.cleaned_data['password']
+                user = authenticate(username = uname, password=upass)
+                if user is not None:
+                    login(request, user)
+                    messages.success(request,"Login Successfully...!!!")
+                    return HttpResponseRedirect('/profile/')
+        else:
+            fm = AuthenticationForm()
+        return render(request, 'enroll/userlogin.html',{'forms':fm,'user':request.user})
+    return HttpResponseRedirect('/profile/')
+    
 
+
+#user logout view
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect('/login/')
 
 #Profile View
 def user_profile(request):
-    return render(request, 'enroll/profile.html')
+    if request.user.is_authenticated:
+        return render(request, 'enroll/profile.html')
+    else:
+        return HttpResponseRedirect('/login/')
+
+
+    
